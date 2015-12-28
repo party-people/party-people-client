c79cb8e1f1f90ce3e385a8cda394fcdfc75c9707
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-12-28 15:38:47 +0900

    Add gulp task to minify HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
 var gulp = require('gulp');
+var minifyHTML  = require('gulp-minify-html');
+var plumber     = require('gulp-plumber');
 
-gulp.task('default', []);
+gulp.task(
+  'compile-html',
+  function() {
+    gulp.src('./src/html/index.html')
+      .pipe(plumber())
+      .pipe(minifyHTML({
+        conditionals: true,
+        spare:true
+      }))
+      .pipe(gulp.dest('./dist'));
+  }
+);
+
+gulp.task('compile', ['compile-html'])
+gulp.task('default', ['compile']);
