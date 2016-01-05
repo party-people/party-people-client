a6742a43b13a49ddd27c252e85b5d56addb74905
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-05 13:44:01 +0900

    Introduce browser history

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -3,19 +3,22 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Router } from 'react-router';
 import { createStore } from 'redux';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import App from './containers/App';
 import Top from './containers/Top';
 import rootReducer from './reducers'
 
 const store = createStore(rootReducer);
+const history = createBrowserHistory();
 const routes = (
   <Route component={ App }>
     <Route path="/" component={ Top } />
   </Route>
 );
+
 render(
   <Provider store={ store }>
-    <Router>{routes}</Router>
+    <Router history={history}>{routes}</Router>
   </Provider>,
   document.getElementById('root')
 );
