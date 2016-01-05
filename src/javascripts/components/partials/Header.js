e9146a89ff174fe3a01ec9ea115b014becdbe3dd
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-05 12:41:40 +0900

    Create header component

---
+++ src/javascripts/components/partials/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header id="header" className="header">
+        <div className="header__inner">
+          <h1 className="header__logo">
+            <a href="/">
+              LOGO
+            </a>
+          </h1>
+          <div className="header__search">
+            <form className="search__form">
+              <input className="search__input" type="text" placeholder="気になるワードを入力" />
+              <div className="search__submit">
+                <input className="search__button" type="submit" value="" />
+                <i className="fa fa-search search__icon" />
+              </div>
+            </form>
+          </div>
+          <ul className="header__menu clearfix">
+            <li className="header__item">
+              <a href="/edit.html">まとめ作成</a>
+            </li>
+            <li className="header__item">
+              <a href="/signup.html">会員登録</a>
+            </li>
+            <li className="header__item">
+              <a href="#">ログイン</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+}
--- src/javascripts/containers/App.js
+++ src/javascripts/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/partials/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
       </div>
     );
   }
