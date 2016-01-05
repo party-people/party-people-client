88bf4be20af56d8130126b01e61e8733474ae651
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-05 13:20:10 +0900

    Extract item component from timeline

--- src/javascripts/components/top/Timeline.js
+++ src/javascripts/components/top/Timeline.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import TimelineItem from '../partials/TimelineItem';
 
 export default class Timeline extends Component {
   render() {
-    const itemImageStyle = {
-      'backgroundImage': 'url(http://content.mery.jp/1100x2000/images/1450564/large.png/original)'
-    };
-    const avatarStyle = {
-      'backgroundImage': 'url(http://dqaeric34olch.cloudfront.net/lists/193783/11351862_519618564846099_858988870_n.jpg/thumb?1451200977)'
-    };
+    const itemNodes = [1,2,3,4,5].map( (item) =>
+      <TimelineItem key={ item } />
+    );
     return(
       <div id="timeline">
         <div className="timeline__inner">
@@ -16,80 +14,7 @@ export default class Timeline extends Component {
             <p>今日注目されている記事の一覧</p>
           </header>
           <ul className="timeline__list">
-            <li className="timeline__item item">
-              <a href="/article.html">
-                <div className="item__inner clearfix">
-                  <div className="item__image" style={itemImageStyle}></div>
-                  <div className="item__content">
-                    <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な美髪をなびかせましょ♡</h5>
-                    <p className="item__description">黒髪にしました。でも、黒髪って重くてダメージが目立つような...</p>
-                    <div className="item__info clearfix">
-                      <div className="item__contributor clearfix">
-                        <div className="contributor__avator" style={avatarStyle}></div>
-                        <p className="contributor__name">tsubasa_honda</p>
-                      </div>
-                      <p className="item__views">
-                        <span className="views">12345</span>view
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </li>
-            <li className="timeline__item item">
-              <div className="item__inner clearfix">
-                <div className="item__image" style={itemImageStyle}></div>
-                <div className="item__content">
-                  <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な美髪をなびかせましょ♡</h5>
-                  <p className="item__description">黒髪にしました。でも、黒髪って重くてダメージが目立つような...</p>
-                  <div className="item__info clearfix">
-                    <div className="item__contributor clearfix">
-                      <div className="contributor__avator" style={avatarStyle}></div>
-                      <p className="contributor__name">tsubasa_honda</p>
-                    </div>
-                    <p className="item__views">
-                      <span className="views">12345</span>view
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li className="timeline__item item">
-              <div className="item__inner clearfix">
-                <div className="item__image" style={itemImageStyle}></div>
-                <div className="item__content">
-                  <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な美髪をなびかせましょ♡</h5>
-                  <p className="item__description">黒髪にしました。でも、黒髪って重くてダメージが目立つような...</p>
-                  <div className="item__info clearfix">
-                    <div className="item__contributor clearfix">
-                      <div className="contributor__avator" style={avatarStyle}></div>
-                      <p className="contributor__name">tsubasa_honda</p>
-                    </div>
-                    <p className="item__views">
-                      <span className="views">12345</span>view
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li className="timeline__item item">
-              <div className="item__inner clearfix">
-                <div className="item__image" style={itemImageStyle}></div>
-                <div className="item__content">
-                  <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な美髪をなびかせましょ♡</h5>
-                  <p className="item__description">黒髪にしました。でも、黒髪って重くてダメージが目立つような...</p>
-                  <div className="item__info clearfix">
-                    <div className="item__contributor clearfix">
-                      <div className="contributor__avator" style={avatarStyle}></div>
-                      <p className="contributor__name">tsubasa_honda</p>
-                    </div>
-                    <p className="item__views">
-                      <span className="views">12345</span>view
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </li>
+            { itemNodes }
           </ul>
         </div>
       </div>
