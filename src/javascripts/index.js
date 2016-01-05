269cae491b6ebbc6b1d4a278003aacf6ffd072ee
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-05 12:36:15 +0900

    Create entry point of redux application

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Router } from 'react-router';
+import { createStore } from 'redux';
+import App from './containers/App';
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
