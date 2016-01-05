cb1aa58abf675c4e758c0e1de64aea63536dbf05
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-05 13:29:52 +0900

    Extract item component from ranking component

--- src/javascripts/components/top/Ranking.js
+++ src/javascripts/components/top/Ranking.js
@@ -1,59 +1,17 @@
 import React, { Component } from 'react';
 import RankingHeader from './RankingHeader';
+import ListItem from '../partials/ListItem';
 
 export default class Ranking extends Component {
   render() {
-    const itemImageStyle = {
-      'backgroundImage': 'url(http://content.mery.jp/1100x2000/images/1450564/large.png/original)'
-    };
+    const itemNodes = [1,2,3,4,5].map( (item) =>
+      <ListItem key={ item } />
+    );
     return(
       <div className="side__content">
         <RankingHeader />
         <ul className="side__content__items">
-          <li className="item list__item">
-            <div className="item__inner clearfix">
-              <div className="item__image" style={itemImageStyle}></div>
-              <div className="item__content">
-                 <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な...</h5>
-                  <div className="item__contributor">
-                    <p className="contributor__name">tsubasa_honda</p>
-                  </div>
-              </div>
-            </div>
-          </li>
-          <li className="item list__item">
-            <div className="item__inner clearfix">
-              <div className="item__image" style={itemImageStyle}></div>
-              <div className="item__content">
-                 <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な...</h5>
-                  <div className="item__contributor">
-                    <p className="contributor__name">tsubasa_honda</p>
-                  </div>
-              </div>
-            </div>
-          </li>
-          <li className="item list__item">
-            <div className="item__inner clearfix">
-              <div className="item__image" style={itemImageStyle}></div>
-              <div className="item__content">
-                 <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な...</h5>
-                  <div className="item__contributor">
-                    <p className="contributor__name">tsubasa_honda</p>
-                  </div>
-              </div>
-            </div>
-          </li>
-          <li className="item list__item">
-            <div className="item__inner clearfix">
-              <div className="item__image" style={itemImageStyle}></div>
-              <div className="item__content">
-                 <h5 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な...</h5>
-                  <div className="item__contributor">
-                    <p className="contributor__name">tsubasa_honda</p>
-                  </div>
-              </div>
-            </div>
-          </li>
+          { itemNodes }
         </ul>
       </div>
     );
