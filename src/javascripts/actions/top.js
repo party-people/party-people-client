17e575a2736e9c839fadd6a64fa40519cdb354f4
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-07 17:41:25 +0900

    Create action for top page

---
+++ src/javascripts/actions/top.js
@@ -0,0 +1,27 @@
+export const FETCH_TOP_REQUEST = 'FETCH_TOP_REQUEST';
+export const FETCH_TOP_SUCCESS = 'FETCH_TOP_SUCCESS';
+export const FETCH_TOP_FAILURE = 'FETCH_TOP_FAILURE';
+
+function fetchTopRequest() {
+  return {
+    type: FETCH_TOP_REQUEST
+  }
+}
+
+
+function fetchTopSuccess() {
+  return {
+    type: FETCH_TOP_SUCCESS
+  }
+}
+
+
+function fetchTopFailure() {
+  return {
+    type: FETCH_TOP_FAILURE
+  }
+}
+
+export function fetchTop() {
+  console.log('fetch top');
+}
