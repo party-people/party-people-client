70e8211a4de54fcb751f04ae1a695fbedb66bd70
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-07 19:03:45 +0900

    Install logger middleware

--- src/javascripts/store/configureStore.js
+++ src/javascripts/store/configureStore.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import rootReducer from '../reducers'
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers';
+
+const loggerMiddleware = createLogger();
 
 const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
+  thunkMiddleware,
+  loggerMiddleware
 )(createStore);
 
 export default function configureStore(initialState) {
