7287e6496fa5aa034e08d2b636b6f7ce7c4c03fd
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-08 20:42:32 +0900

    Embed date into pick item component

--- src/javascripts/components/top/PickupItem.js
+++ src/javascripts/components/top/PickupItem.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import If from '../utilities/If';
 
 export default class PickupItem extends Component {
   render() {
+    const {
+      index,
+      article: { title, description, views }
+    } = this.props;
     const itemImageStyle = {
       'backgroundImage': 'url(http://content.mery.jp/1100x2000/images/1456000/gal-meets-glam-stuart-weitzman-gilligan7.jpg/original)'
     };
@@ -15,9 +19,9 @@ export default class PickupItem extends Component {
         <div className="item__content">
           <div className="item__content__wrapper">
             <div className="item__content__inner">
-              <h3 className="item__title">黒髪にしたのなら。ヒロイン級の”さらさら天使の輪”な美髪をなびかせましょ♡</h3>
-              <If show={false}>
-                <p className="item_description">黒髪にしました。でも、黒髪って重くてダメージが目立つような…なりたいのは漫画のヒロインのような天使の輪が光るさらさら髪。そんな美髪になるために毎日ヘアケアを行いましょう。大変なヘアケアではなく毎日のケアを...</p>
+              <h3 className="item__title">{ title }</h3>
+              <If show={index == 0}>
+                <p className="item_description">{ description }</p>
               </If>
               <div className="item__info clearfix">
                 <div className="item__contributor clearfix">
@@ -25,7 +29,7 @@ export default class PickupItem extends Component {
                   <p className="contributor__name">tsubasa_honda</p>
                 </div>
                 <div className="item__views">
-                  <span className="views">112345</span>view
+                  <span className="views">{ views }</span>view
                 </div>
               </div>
             </div>
@@ -34,4 +38,13 @@ export default class PickupItem extends Component {
       </div>
     );
   }
+}
+
+PickupItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  article: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    views: PropTypes.number.isRequired
+  }).isRequired
 }
