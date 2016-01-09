0533990a76d243af4d0246ad21b42286147e659a
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-09 22:41:11 +0900

    Give the redux-form reducer to Redux

--- src/javascripts/reducers/index.js
+++ src/javascripts/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import entities from './entities';
 import current_user from './current_user';
 import top from './top';
+import { reducer as form } from 'redux-form';
 
 const rootReducer = combineReducers({
   entities,
   current_user,
-  top
+  top,
+  form
 });
 
 export default rootReducer;
