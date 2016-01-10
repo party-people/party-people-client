3b85e544272f830abe239d97c046cd02b662b0e4
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-10 12:52:06 +0900

    Implement update article actions

--- src/javascripts/actions/articles.js
+++ src/javascripts/actions/articles.js
@@ -1,9 +1,39 @@
 import fetcher from '../fetcher';
 
 export const CREATE_NEW_ARTICLE = 'CREATE_NEW_ARTICLE';
+export const UPDATE_ARTICLE_REQUEST = 'UPDATE_ARTICLE_REQUEST';
+export const UPDATE_ARTICLE_SUCCESS = 'UPDATE_ARTICLE_SUCCESS';
+export const UPDATE_ARTICLE_FAILURE = 'UPDATE_ARTICLE_FAILURE';
 
 export function createNewArticle() {
   return {
     type: CREATE_NEW_ARTICLE
   };
+}
+
+function updateArticleRequest() {
+  return {
+    type: UPDATE_ARTICLE_REQUEST
+  }
+}
+
+function updateArticleSuccess(body) {
+  return {
+    type: UPDATE_ARTICLE_SUCCESS
+  }
+}
+
+function updateArticleFailure(ex) {
+  return {
+    type: UPDATE_ARTICLE_FAILURE
+  }
+}
+
+export function updateArticle(data) {
+  return dispatch => {
+    dispatch(updateArticleRequest());
+    return fetcher.patch('http://localhost:9000/api/v1/articles/1', data)
+      .then(json => dispatch(updateArticleSuccess(json.body)))
+      .catch(ex => dispatch(updateArticleFailure(ex)))
+  }
 }
