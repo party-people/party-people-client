ff3153d6597351a97ead15d220ce90e388c1d975
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-10 17:25:59 +0900

    Implement to call create user request when submit sign up form

--- src/javascripts/containers/Session.js
+++ src/javascripts/containers/Session.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
+import { postUser } from '../actions/users';
 import SessionForm from '../components/session/SessionForm';
 
-export default class Session extends Component {
+class Session extends Component {
+  handleSessionSubmit(data) {
+    this.props.dispatch(postUser(JSON.stringify({user: data})));
+  }
+
   render() {
     return(
       <div id="session">
         <div className="session__mask">
         </div>
         <div className="session__inner">
-          <SessionForm />
+          <SessionForm onSubmit={this.handleSessionSubmit.bind(this)} />
         </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return state
+}
+
+export default connect(mapStateToProps)(Session);
