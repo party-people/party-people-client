9bfcfb08cd7d0c9193e05ef84f5ad241c4612f1b
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-10 12:51:47 +0900

    Replace redux form component from html form

--- src/javascripts/components/editor/EditorInfo.js
+++ src/javascripts/components/editor/EditorInfo.js
@@ -1,23 +1,26 @@
 import React, { Component, PropTypes } from 'react';
+import Input from '../form/Input';
+import FileInput from '../form/FileInput';
+import TextArea from '../form/TextArea';
 
 export default class EditorInfo extends Component {
   render() {
     const { 
-      article: { title, description, thumbnailUrl}
+      article, fields: { title, description, thumbnail }
     } = this.props;
     return(
       <div id="editor-info">
         <div className="info__inner clearfix">
           <div className="info__image">
-            <input className="image__input" type="file" name="article[thumbnail]" />
+            <FileInput className="image__input" field={thumbnail} />
             <img className="article__image" src="http://miyagifukkououentai.jp/wp/wp-content/themes/ifeaturepro/noimage.jpg" />
           </div>
           <div className="info__text">
             <div className="info__title">
-              <input className="title__input" type="text" name="article[title]" placeholder="まとめのタイトル" value={title} />
+              <Input className="title__input" field={title} placeholder="まとめのタイトル" defaultValue={article.title} />
             </div>
             <div className="info__description">
-              <textarea className="description__textarea" name="article[description]" placeholder="まとめの説明（160文字以内）" value={description}></textarea>
+              <TextArea className="description__textarea" field={description} placeholder="まとめの説明（160文字以内）" defaultValue={article.description} />
             </div>
           </div>
         </div>
@@ -31,5 +34,6 @@ EditorInfo.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     thumbnailUrl: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  fields: PropTypes.object.isRequired
 }
