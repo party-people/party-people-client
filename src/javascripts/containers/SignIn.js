5fa29b8ee65a10712c4e07379264d3d14f26d616
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-13 11:43:55 +0900

    Setting client params when submit sign in request

--- src/javascripts/containers/SignIn.js
+++ src/javascripts/containers/SignIn.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
-import { postUser } from '../actions/users';
+import { postOauthToken } from '../actions/users';
 import SignInForm from '../components/session/SignInForm';
 
 class SignIn extends Component {
   handleSessionSubmit(data) {
-    console.log(data);
+    data.grant_type = 'password'
+    data.client_id = '3ac7d043d8bf1b90951d1e2d4af9d69e153d335783b9bda33da14ed49241d57a'
+    data.client_secret = '1b0cb5dbc118518b438b64d6be1e2ef9099703a336f72cd352ec096d94554352'
+    this.props.dispatch(postOauthToken(JSON.stringify(data)));
   }
 
   render() {
